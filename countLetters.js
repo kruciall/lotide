d038d3ff6cf17b439892b03ba7d3534df9e978f3
Vinson Wong <vinson.wong93@gmail.com>
2022-09-21 15:30:55 -0400

    created countLetters.js and implemented function to count all letters

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const string = "We are the champions Lighthouse in the house";
+const countLetters = function(string) {
+  const obj = {};
+  const lowercaseString = string.toLowerCase();
+  for (let c of lowercaseString) {
+    if (c !== " ") {
+      if (!obj[c]) {
+        obj[c] = 0;
+      }
+      obj[c]++;
+    }
+  }
+  return obj;
+};
+
+console.log(countLetters(string));
