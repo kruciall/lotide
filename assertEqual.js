61a60d1a89f1e4e57eb4f5e4b9a6731436eece62
Vinson Wong <vinson.wong93@gmail.com>
2022-09-23 17:31:03 -0400

    refactored code and eliminated the else statement

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,8 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
-  } else {
+  if (actual !== expected ) {
     console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
+    return;
   }
+  
+  console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
 };
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(5,"boot");
