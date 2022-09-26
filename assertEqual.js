db35bf1ade6381395d43136e1e0fb26d78c9fca3
Vinson Wong <vinson.wong93@gmail.com>
2022-09-26 13:25:47 -0400

    refactored assertEqual.js and head.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,10 @@
 const assertEqual = function(actual, expected) {
-  if (actual !== expected ) {
+  if (actual !== expected) {
     console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
     return;
   }
-  
+
   console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
 };
+
+module.exports = assertEqual;
