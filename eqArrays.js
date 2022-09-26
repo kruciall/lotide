af257d64670ddc1f38262ff8dc4c9add370662f9
Vinson Wong <vinson.wong93@gmail.com>
2022-09-26 13:50:19 -0400

    refactored eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 const eqArrays = function(arr1,arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -18,9 +9,4 @@ const eqArrays = function(arr1,arr2) {
   }
   return true;
 };
-
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
