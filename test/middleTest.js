7644f9381645437c877cf7ff66b6a0d38a35fc29
Vinson Wong <vinson.wong93@gmail.com>
2022-09-26 16:56:48 -0400

    added mocha and chai to middle.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,19 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1]));
-console.log(middle([1,2]));
+describe("#middle", () => {
+  it("[2,3]", () => {
+    const arr = [1, 2, 3, 4];
+    assert.deepEqual(middle(arr), [2,3]);
+  });
+
+  it("[3,4]", () => {
+    const arr = [1, 2, 3, 4, 5, 6];
+    assert.deepEqual(middle(arr), [3,4]);
+  });
+
+  it("[]", () => {
+    const arr = [1];
+    assert.deepEqual(middle(arr), []);
+  });
+});
