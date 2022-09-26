c94e0dcf24a09483277678d5f9a2c31e71a97696
Vinson Wong <vinson.wong93@gmail.com>
2022-09-26 13:39:52 -0400

    removed assertEqual function (forgot it previously)

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌  Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   let newArray = array.slice(1);
   return (console.log(newArray));
