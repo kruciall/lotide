466eb3091329f82401b2bf6d0f89f472bae426c2
Vinson Wong <vinson.wong93@gmail.com>
2022-09-26 16:51:03 -0400

    added mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,21 +1,14 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail.js');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
+describe("#tail", () => {
+  it("returns [Two]", () => {
+    const words = ["One", "Two"];
+    assert.deepEqual(tail(words), ["Two"]);
+  });
 
-tail(words);
-assertEqual(words.length, 3);
-assertArraysEqual(tail(words), ["Lighthouse", "Labs"]);
-
-const word = ["One"];
-tail(word);
-assertEqual(word.length, 1);
-assertArraysEqual(tail(word), ["One"]);
-
-
-
-const empty = [];
-tail(empty);
-assertEqual(empty.length, 0);
-assertArraysEqual(tail(empty), []);
+  it("returns the last two elements in the array, Lighthouse, Labs", () => {
+    const words = ["Yo yo", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(words), ["Lighthouse", "Labs"]);
+  });
+});
